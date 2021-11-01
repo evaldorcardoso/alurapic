73e60f9c963e9acc1bd083153eea4a42931f8838
Evaldo R. C <evaldorubel@hotmail.com>
2021-11-01 09:21:55 -0300

    First commit - Aula 02

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'Foto 1'
+        },
+        {
+          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'Foto 2'
+        }
+      ]      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
